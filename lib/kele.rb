57ac7eab5f53ef4f9ac69d76a2e2f131dc1c8cc9
Daniela Sannino <danielasannino@Danielas-MacBook-Pro.local>
2017-10-15 20:37:43 -0400

    assignment 3

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,19 +1,26 @@
-+require 'httparty'
-+
-+class Kele
-+  include HTTParty
-+
-+  base_uri 'https://www.bloc.io/api/v1'
-+
-+  def initialize(email, password)
-+    post_response = self.class.post('/sessions', body: {
-+        email: email,
-+        password: password
-+      })
-+    @user_auth_token = post_response['auth_token']
-+    raise "Invalid Email or Password. Try Again." if @user_auth_token.nil?
-+
-+  end
-+
-+
-+end
+require 'httparty'
+require 'json'
+
+class Kele
+  include HTTParty
+
+  base_uri 'https://www.bloc.io/api/v1'
+
+  def initialize(email, password)
+    post_response = self.class.post('/sessions', body: {
+        email: email,
+        password: password
+      })
+    @user_auth_token = post_response['auth_token']
+    raise "Invalid Email or Password. Try Again." if @user_auth_token.nil?
+  end
+
+  def get_me
+    response = self.class.get('/users/me', headers: { "authorization" => @user_auth_token })
+    hash = JSON.parse(response.body)
+    puts hash['name']
+
+  end
+
+
+end
